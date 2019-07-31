fbfb70419e1bfeced0b98a2521b61138e42b0551
Vijendra Kumar <43384082+vijevira@users.noreply.github.com>
2019-07-31 19:21:16 +0530

    Create Assignment1.py

---
+++ python/Assignment1.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+print("a+b = ",a+b)
+print("a-b = ",a-b)
+print("a*b = ",a*b)
+try:
+    print("a/b = ",a/b)
+except:
+    print("division by zero exception")
